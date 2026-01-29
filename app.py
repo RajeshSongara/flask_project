ffc20c1be99dc27f605a74a92bea22b59ca221fa
RajeshSongara <rajsongara121@gmail.com>
2026-01-29 11:02:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template
+
+post =[
+    
+    {
+        'author':'corey schafer',
+        'title':'blog post 1',
+        'content':'first post content',
+        'date_posted':'apirl 20 ,2018'
+    },
+    {
+        'author':'Rajesh',
+        'title':'blog post 2',
+        'content':'secound post content',
+        'date_posted':'apirl 21 ,2018'
+    },
+]
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
